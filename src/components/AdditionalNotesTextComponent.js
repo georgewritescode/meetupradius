d41146d4fb0d811a67dcba43f2079990417e8c78
George <grmcameron@gmail.com>
2021-08-18 12:35:41 +1000

    Remove contact for spam purposes

--- src/components/AdditionalNotesTextComponent.js
+++ src/components/AdditionalNotesTextComponent.js
@@ -9,7 +9,6 @@ export default () => <div className="text-sm w-full text-left mt-8 text-gray-500
 				<li>Locations may not be precise and are rounded to a precision of ~100m</li>
 				<li>Your location data is private and is not stored on our servers. It is stored on your web browser</li>
 				<li>This website may not be updated for the latest regulations</li>
-			<li>Contact: georgecameronpersonal @ (popular email service run by Google) </li>
 			</ul>
 		</div>
 </div>
